e593894d2ec5e1459460d564e53a67c3cd247cf1
Tam Nhu Tran <kaitran.ntt@gmail.com>
2022-05-13 12:27:29 -0400

    Adding Question model - TODO: Updating Constants from hardcode to fetch some API?

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etName = findViewById<AppCompatEditText>(R.id.et_Name)
+        val btnStart = findViewById<Button>(R.id.btn_Start)
+
+        btnStart.setOnClickListener {
+            if (etName.text.toString().isEmpty()) {
+                etName.error = "Please enter your name"
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
